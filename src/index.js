5255a1da0467974d5582a47b6c0e24d0dade6441
Venelin Venkov <venelin.venkov@protonmail.com>
2025-09-18 18:14:36 +0200

    Add and config handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log('it works')
+import express from 'express';
+import handlebars from 'express-handlebars';
+
+// Init express instance
+const app = express();
+
+// Add a config view engine
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+//Set default engine
+app.set('view engine', 'hbs');
+
+// Config routes
+app.get('/', (req, res) => {
+    res.render('home', { layout: false });
+});
+
+//Start express web server 
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'))

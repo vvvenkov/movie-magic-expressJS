066ad11a9bf7765a1a4637910f927935f9587056
Venelin Venkov <venelin.venkov@protonmail.com>
2025-09-19 15:53:12 +0200

    Add home controller

---
+++ src/controllers/homeController.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const homeController = express.Router();
+
+homeController.get('/', (req, res) => {
+    res.render('home');
+});
+homeController.get('/about', (req, res) => {
+    res.render('about');
+})
+
+export default homeController;

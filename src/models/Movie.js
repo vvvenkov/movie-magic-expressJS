bd6c4b2da6870202d945e735ec98d734ad1b2864
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-10-06 12:09:27 +0200

    add validation to movie model

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,14 +1,44 @@
 import { Schema, model } from 'mongoose';
 
+const maxYearAllowed = new Date().getFullYear() + 5;
+
+
 const movieSchema = new Schema({
-    title: String,
-    category: String,
-    genre: String,
-    director: String,
-    year: Number,
-    imageUrl: String,
-    rating: Number,
-    description: String,
+    title: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+    },
+    category: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+    },
+    genre: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+    },
+    director: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+    },
+    year: {
+        type: Number,
+        required: [true, 'imageUrl is required!'],
+        min: 1970,
+        max: [maxYearAllowed, `Year cannot be larger than ${maxYearAllowed}!`],
+    },
+    imageUrl: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+    },
+    rating: {
+        type: Number,
+        required: [true, 'imageUrl is required!'],
+    },
+    description: {
+        type: String,
+        required: [true, 'imageUrl is required!'],
+        maxLength: [100, 'Description is too long!'],
+    },
 });
 
 const Movie = model('Movie', movieSchema);

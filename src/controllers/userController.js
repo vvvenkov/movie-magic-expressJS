87e863f0daf197e52e7d4db6260633e6537d1e6a
Venelin Venkov <112377436+vvvenkov@users.noreply.github.com>
2025-10-18 11:56:57 +0200

    Register user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
+import userService from "../services/userService.js";
 
 const userController = Router();
 
-userController.get('/register', (req, res) =>{
+userController.get('/register', (req, res) => {
     res.render('user/register');
 })
+
+userController.post('/register', async (req, res) => {
+    // Get data from request
+    const userData = req.body;
+
+    //Register user
+    await userService.register(userData)
+
+    //Redirect to login
+    res.redirect('/users/login')
+});
 
 export default userController;
